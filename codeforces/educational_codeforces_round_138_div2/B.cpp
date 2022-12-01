784dbf423a8cf1dedd9f2eb21ff6c0d1afaceddb
drew <58788781+drew138@users.noreply.github.com>
2022-11-30 21:03:50 -0500

    simulated educational codeforces round 138

---
+++ codeforces/educational_codeforces_round_138_div2/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;  // int vec
+typedef vector<vi> vvi;  // int vec vec
+typedef vector<pii> vpi; // int int pair vec
+
+const double pi = 3.1415926535897932384626433832795;
+const int maxn = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ll INFLL = 1000000000000000005LL;
+const ld EPS = 1e-9;
+int dirx[8] = {-1, 0, 0, 1, -1, -1, 1, 1};
+int diry[8] = {0, 1, -1, 0, -1, 1, -1, 1};
+
+#define d(x) cout << #x << " = " << x << endl;
+#define ios ios_base::sync_with_stdio(0), cin.tie(0);
+#define forn(i, n) for (int i = 0; i < (int)n; ++i)
+#define forab(i, a, b) for (int i = a; i < (int)b; ++i)
+#define foreach(a, b) for (auto &(a) : (b))
+#define formap(map) for (const auto &[key, value] : map)
+#define all(v) v.begin(), v.end()
+#define allar(arr, sz) arr, arr + sz
+#define ms(ar, val) memset(ar, val, size)
+#define pq(type) priority_queue<type>
+#define pqd(type) priority_queue<type, vector<type>, greater<type>>
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define nl "\n"
+#define in(t) while (t--)
+
+void solve();
+
+int main() { solve(); }
+
+#define int long long
+
+void solve() {
+  ios int t;
+  cin >> t;
+  in(t) {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    forn(i, n) { cin >> a[i]; }
+    forn(i, n) { cin >> b[i]; }
+
+    int i = 0, j = n - 1;
+    int ans = 0;
+    while (i < j) {
+      if (b[i] < b[j]) {
+        ans += a[i];
+        ans += b[i];
+        i++;
+      } else {
+        ans += a[j];
+        ans += b[j];
+        j--;
+      }
+    }
+    ans += a[i];
+
+    cout << ans << nl;
+  }
+}
---
+++ codeforces/educational_codeforces_round_138_div2/C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;  // int vec
+typedef vector<vi> vvi;  // int vec vec
+typedef vector<pii> vpi; // int int pair vec
+
+const double pi = 3.1415926535897932384626433832795;
+const int maxn = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ll INFLL = 1000000000000000005LL;
+const ld EPS = 1e-9;
+int dirx[8] = {-1, 0, 0, 1, -1, -1, 1, 1};
+int diry[8] = {0, 1, -1, 0, -1, 1, -1, 1};
+
+#define d(x) cout << #x << " = " << x << endl;
+#define ios ios_base::sync_with_stdio(0), cin.tie(0);
+#define forn(i, n) for (int i = 0; i < (int)n; ++i)
+#define forab(i, a, b) for (int i = a; i < (int)b; ++i)
+#define foreach(a, b) for (auto &(a) : (b))
+#define formap(map) for (const auto &[key, value] : map)
+#define all(v) v.begin(), v.end()
+#define allar(arr, sz) arr, arr + sz
+#define ms(ar, val) memset(ar, val, size)
+#define pq(type) priority_queue<type>
+#define pqd(type) priority_queue<type, vector<type>, greater<type>>
+#define umap unordered_map
+#define uset unordered_set
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define nl "\n"
+#define in(t) while (t--)
+
+void solve();
+
+int main() { solve(); }
+
+#define int long long
+
+bool is_valid(vector<int> arr, int k) {
+  forn(i, k) {
+    while (arr.size() && arr[arr.size() - 1] > (k - i))
+      arr.pop_back();
+    if (!arr.size()) {
+      return false;
+    }
+    arr.pop_back();
+    if (i < arr.size()) {
+      arr[i] += k - i;
+    }
+  }
+  return true;
+}
+
+int binary_search(vector<int> &arr) {
+  int l = 0, r = arr.size() + 1;
+  while (l + 1 < r) {
+    int mid = (r - l) / 2 + l;
+    bool v = is_valid(arr, mid);
+    if (v) {
+      l = mid;
+    } else {
+      r = mid;
+    }
+  }
+  return l;
+}
+
+void solve() {
+  ios int t;
+  cin >> t;
+  in(t) {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    forn(i, n) cin >> arr[i];
+    sort(all(arr));
+    int ans = binary_search(arr);
+    cout << ans << nl;
+  }
+}
